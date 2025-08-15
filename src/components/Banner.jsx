d61ee91db43ff64b6be6a146d1d43ebad059c4fa
Terrier Simon <simonterrier110994@gmail.com>
2025-08-15 12:20:04 +0200

    amelioration header banner footer page acceuil

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -3,7 +3,6 @@ export default function Banner({ image, text }) {
     <div className="banner">
       <div className="banner__inner">
         <img src={image} alt={text} className="banner__image" />
-        <div className="banner__overlay"></div>
         {text && <h1 className="banner__text">{text}</h1>}
       </div>
     </div>

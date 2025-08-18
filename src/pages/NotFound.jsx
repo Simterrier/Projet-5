cdefd0a4a29cbad58d4a845d789a2f5b90eb9b46
Terrier Simon <simonterrier110994@gmail.com>
2025-08-18 16:11:14 +0200

    responsive page not found

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -4,9 +4,9 @@ export default function NotFound() {
   return (
     <section className="notfound" aria-labelledby="notfound-title">
       <h1 id="notfound-title" className="notfound__code">404</h1>
-      <p className="notfound__msg">
-        Oups! La page que vous demandez n'existe pas.
-      </p>
+     <p className="notfound__msg">
+  Oups ! La page que{"\n"} vous demandez n’existe pas.
+</p>
       <Link to="/" className="notfound__link">
         Retourner sur la page d’accueil
       </Link>
